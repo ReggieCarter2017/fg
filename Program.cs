1c604201eadc4548f9ee75c850349081868502f0
ReggieCarter <coolpagani@gmail.com>
2022-09-22 11:00:13 +0300

    Array sorting created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] sortArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (array[i].Length < 4) newArray[i] = array[i];
+    }
+    return newArray;
+}
