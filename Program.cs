431fb9cee448361901bf28e76db03be82f2e4322
ReggieCarter <coolpagani@gmail.com>
2022-09-22 11:09:14 +0300

    PrintArray method created

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿string[] sortArray(string[] array)
+﻿
+string[] sortArray(string[] array)
 {
     string[] newArray = new string[array.Length];
     for (int i = 0; i < newArray.Length; i++)
@@ -7,3 +8,13 @@
     }
     return newArray;
 }
+string[] a = {"12", "Paper", " ..213", "Peq", " .1"};
+
+void PrintArray(string[] b)
+{
+    foreach (var el in b)
+    {
+        Console.Write(el + " ");
+    }
+}
+PrintArray(sortArray(a));
